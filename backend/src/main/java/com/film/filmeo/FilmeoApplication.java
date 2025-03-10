57208de8a41213d53797dd663da5cb5ed392ed2b
steo <nguyenstephanie@live.fr>
2025-03-10 15:49:56 +0100

    update3

---
+++ backend/src/main/java/com/film/filmeo/FilmeoApplication.java
@@ -0,0 +1,13 @@
+package com.film.filmeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/film/filmeo/FilmeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.film.filmeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
